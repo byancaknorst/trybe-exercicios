3c41a54f178373413e207e7bde1de5822951be3b
Byanca Knorst <byancaknorst@gmail.com>
2021-11-19 15:41:04 -0300

    exercitando

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/script.js
@@ -0,0 +1,3 @@
+//console.log("Oi");
+let tagP =  document.getElementsByTagName('p')[1];
+tagP.innerText = "Meu futuro depende de mim, daqui a dois anos pretendo estar estabilizada e trabalhando na XP";
