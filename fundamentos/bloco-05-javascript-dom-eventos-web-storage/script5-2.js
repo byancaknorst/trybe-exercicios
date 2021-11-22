d7cee39dff87c56d71799d88f1f9b4353e964e28
Byanca Knorst <byancaknorst@gmail.com>
2021-11-21 22:46:00 -0300

    exercitando

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/script5-2.js
+++
@@ -1,5 +0,0 @@
-let corpo = document.querySelector('body');
-const titulo = document.createElement('h1');
-corpo.appenChild = titulo;
-console.log(corpo)
-titulo.innerText = "Exercício 5.2 - JavaScript DOM";
